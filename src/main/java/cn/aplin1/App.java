67e539f3bef925aa93cf9458e1f2a6c834fc3181
T470 <T470@DESKTOP-5PRL4HH>
2018-08-24 15:17:44 +0800

    static

--- src/main/java/cn/aplin1/App.java
+++ src/main/java/cn/aplin1/App.java
@@ -3,7 +3,11 @@ package cn.aplin1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * 君当作磐石妾当作蒲苇,蒲苇纫如丝磐石无转移
+ * @author 附耳聆听
+ *
+ */
 @SpringBootApplication
 public class App {
 
