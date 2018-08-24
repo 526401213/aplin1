aab3b648e808c912fcf0067245432a585ff1d0fa
T470 <T470@DESKTOP-5PRL4HH>
2018-08-24 18:58:44 +0800

    des

--- src/main/java/cn/aplin1/controller/UserController.java
+++ src/main/java/cn/aplin1/controller/UserController.java
@@ -1,7 +1,10 @@
 package cn.aplin1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.aplin1.service.UserService;
 
 /**
  * 轻声悠扬弹,不尽长夜的凄凉
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 	
+	@Autowired
+	private UserService uservice;
 	
 	@RequestMapping("/list")
 	public String list() {
@@ -20,5 +25,18 @@ public class UserController {
 		
 		return "";
 	}
+	
+	@RequestMapping("/toAdd")
+	public String toAdd() {
+		
+		return "";
+	}
+	
+	@RequestMapping("/add")
+	public String add() {
+		
+		return "";
+	}
+	
 	
 }
